f3b4da6bb50a185903b78d802b5b5c37bd9e6b57
Emanuele Pisano <emanuele.pisano@mail.polimi.it>
2021-04-03 22:57:00 +0200

    Adaptation to liboqs

--- Additional_Implementations/liboqs/generate.py
+++ Additional_Implementations/liboqs/generate.py
@@ -5,73 +5,19 @@ from pathlib import Path
 
 
 CLEAN_TARGET_FOLDER = "./kem/ledacrypt"
-#NINETIES_TARGET_FOLDER = "./kem/ledacrypt"
 
 LEDACRYPT_SOURCE_FOLDERS = [
     'base/Common',
     'base/KEM'
 ]
 
-LEDACRYPT_FILES = [
-    'LICENSE.TXT',
-    'META.yml',
-    'aes256.c',
-    'aes256.h',
-    'api.h',
-    'bf_decoding.c',
-    'bf_decoding.h',
-    'dfr_test.c',
-    'dfr_test.h',
-    'djbsort.c',
-    'djbsort.h',
-    'gf2x_arith.c',
-    'gf2x_arith.h',
-    'gf2x_arith_mod_xPplusOne.c',
-    'gf2x_arith_mod_xPplusOne.h',
-    'gf2x_limbs.h',
-    'H_Q_matrices_generation.c',
-    'H_Q_matrices_generation.h',
-    'Makefile',
-    'Makefile.Microsoft_nmake',
-    'niederreiter.h',
-    'niederreiter_decrypt.c',
-    'niederreiter_decrypt.h',
-    'niederreiter_encrypt.c',
-    'niederreiter_encrypt.h',
-    'niederreiter_keygen.c',
-    'niederreiter_keygen.h',
-    'qc_ldpc_parameters.h',
-    'rng.c',
-    'rng.h'
+LEDACRYPT_CPA_SOURCE_FOLDERS = [
+    'base/Common',
+    'base/KEM-CPA'
 ]
 
-# LEDACRYPT_CPA_FILES = [
-#     'LICENSE',
-#     'api.h',
-#     'aes256ctr.c',
-#     'aes256ctr.h',
-#     'cbd.c',
-#     'cbd.h',
-#     'indcpa.c',
-#     'indcpa.h',
-#     'kem.c',
-#     'ntt.c',
-#     'ntt.h',
-#     'params.h',
-#     'poly.c',
-#     'poly.h',
-#     'polyvec.c',
-#     'polyvec.h',
-#     'reduce.c',
-#     'reduce.h',
-#     'symmetric.h',
-#     'verify.c',
-#     'verify.h',
-# ]
-
-
 params = [
-    {'name': 'ledacrypt23371',
+    {'name': 'ledacrypt_23371',
      'impl': 'clean',
      'def': ['CATEGORY=1', 'N0=2', 'DFR_SL_LEVEL=0'],
      'undef': [],
@@ -100,7 +46,7 @@ for param in params:
         shutil.rmtree(pqcleanDir)
     os.makedirs(pqcleanDir)
 
-    nmspc = (f"PQCLEAN_{parameterSet.upper().replace('-','')}"
+    nmspc = (f"OQS_KEM_{parameterSet.upper().replace('-','')}"
              f"_{param['impl'].upper()}")
     for src in param['src_folders']:
         for f in os.listdir(src):
@@ -108,12 +54,13 @@ for param in params:
             shutil.copyfile(f"{src}/{f}", f"{pqcleanDir}/{f}")
 
             # namespace source files
-            cmd = f"sed -i 's/PQCLEAN_NAMESPACE/{nmspc}/g' '{pqcleanDir}/{f}'"
+            cmd = f"sed -i 's/OQS_NAMESPACE/{nmspc}/g' '{pqcleanDir}/{f}'"
             print(cmd)
             subprocess.check_call(cmd, shell=True)
 
             # remove preprocessor conditionals
-            cmd = ("unifdef -m " + " ".join(["-D"+d for d in param['def']]) + " "
+            cmd = ("unifdef -m " 
+                + " ".join(["-D"+d for d in param['def']]) + " "
                 + " ".join(['-U'+d for d in param.get('undef', [])])
                 #+ f" -f params/params-{param['name']}.h"
                 + f" {pqcleanDir}/{f}")
@@ -121,13 +68,19 @@ for param in params:
             subprocess.call(cmd, shell=True)
         # copy over param specific files
 
-    for f in ['api.h', 'META.yml']:
+    for f in ['api.h']:
         cmd = "sed -i '" + "".join(["s/"+k+"/"+v+"/g; " for k,v in param['values'].items()]) + "' " + f"'{pqcleanDir}/{f}'"
         subprocess.check_call(cmd, shell=True)
 
-    # replace lib name
-    #cmd = f"sed -i 's/SCHEME_NAME/{parameterSet}/g' {pqcleanDir}/{f}"
-    #subprocess.call(cmd, shell=True)
+
+    # copy over oqs header file
+    shutil.copyfile(f"base/ledacrypt.c", f"{CLEAN_TARGET_FOLDER}/kem_{parameterSet}.c")
+    cmd = f"sed -i 's/GENERATOR_SCHEME_NAME/{parameterSet}/g; s/GENERATOR_NIST_LEVEL/{param['values']['GENERATOR_NIST_LEVEL']}/g' {CLEAN_TARGET_FOLDER}/kem_{parameterSet}.c"
+    subprocess.call(cmd, shell=True)
+
+# copy main header file and cmake
+shutil.copyfile(f"base/kem_ledacrypt.h", f"{CLEAN_TARGET_FOLDER}/kem_ledacrypt.h")
+shutil.copyfile(f"base/CMakeLists.txt", f"{CLEAN_TARGET_FOLDER}/CMakeLists.txt")
 
     # run astyle to fix formatting due to namespace
     #cmd = f"astyle --project {pqcleanDir}/*.[ch]"
